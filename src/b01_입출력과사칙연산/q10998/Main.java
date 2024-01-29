90b80a0f2ec8570b2c9ed743bf3d53577060276c
eun23 <127043779+eun23@users.noreply.github.com>
2024-01-29 22:15:16 +0900

    20240129

---
+++ src/b01_입출력과사칙연산/q10998/Main.java
@@ -0,0 +1,18 @@
+package b01_입출력과사칙연산.q10998;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = 0, b = 0;
+		
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		System.out.println(a * b);
+	}
+
+}
---
+++ src/b01_입출력과사칙연산/q2557/Main.java
@@ -0,0 +1,9 @@
+package b01_입출력과사칙연산.q2557;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
